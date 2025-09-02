3e28887f374f2116acd92539312761e66bc2701b
prem4667 <prem.adegaon@gmail.com>
2025-09-03 00:58:08 +0530

    Create employer.js

---
+++ job/backend/routes/employer.js
@@ -0,0 +1,23 @@
+import express from "express";
+import Employer from "../models/Employer.js";
+
+const router = express.Router();
+
+// Register employer
+router.post("/register", async (req, res) => {
+  try {
+    const newEmp = new Employer(req.body);
+    await newEmp.save();
+    res.json({ message: "Registration submitted successfully" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// Get all employers (for admin)
+router.get("/", async (req, res) => {
+  const employers = await Employer.find();
+  res.json(employers);
+});
+
+export default router;
